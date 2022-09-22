2f28d96fea11657fb8160cd3188dbd5ff5499420
Josh Chappelow <jdchappelow@gmail.com>
2022-09-22 18:51:39 +0900

    click function

--- components/Row.tsx
+++ components/Row.tsx
@@ -2,13 +2,18 @@ import { Movie } from "../typings";
 import { ChevronLeftIcon } from "@heroicons/react/20/solid";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import Thumbnail from "./Thumbnail";
+import { useRef, useState } from "react";
 
 interface Props {
+  // movie: Movie | DocumentData[];
   title: string;
   movies: Movie[];
 }
 
 function Row({ title, movies }: Props) {
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setIsMoved] = useState(false);
+
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
       <h2 className="w-56 cursor-pointer text-sm font-semibold text-white transition duration-200 hover:text-blue-700 md:text-2xl">
@@ -17,13 +22,19 @@ function Row({ title, movies }: Props) {
       <div className="relative md:-ml-2">
         <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
 
-        <div className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
+        <div
+          ref={rowRef}
+          className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2"
+        >
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
 
-        <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+        <ChevronRightIcon
+          className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
