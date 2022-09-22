6e61ea0449c85a3e8e16c928921f09bacb0ad714
Josh Chappelow <jdchappelow@gmail.com>
2022-09-22 19:05:49 +0900

    finally scrolling working

--- components/Row.tsx
+++ components/Row.tsx
@@ -14,13 +14,33 @@ function Row({ title, movies }: Props) {
   const rowRef = useRef<HTMLDivElement>(null);
   const [isMoved, setIsMoved] = useState(false);
 
+  const handleClick = (direction: string) => {
+    setIsMoved(true);
+
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+
+      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
       <h2 className="w-56 cursor-pointer text-sm font-semibold text-white transition duration-200 hover:text-blue-700 md:text-2xl">
         {title}
       </h2>
-      <div className="relative md:-ml-2">
-        <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-1 transition hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && "hidden"
+          }`}
+          onClick={() => handleClick("left")}
+        />
 
         <div
           ref={rowRef}
@@ -32,7 +52,7 @@ function Row({ title, movies }: Props) {
         </div>
 
         <ChevronRightIcon
-          className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-1 transition hover:scale-125 group-hover:opacity-100"
           onClick={() => handleClick("right")}
         />
       </div>
