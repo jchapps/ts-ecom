a37c5ee76a584e8183135a587a7f9c5d9fbbfd11
Josh Chappelow <jdchappelow@gmail.com>
2022-09-22 16:43:11 +0900

    banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,31 @@
-function Banner() {
+import { useEffect, useState } from "react";
+import { Movie } from "../typings";
+import { baseUrl } from "../constants/Movie";
+import Image from "next/image";
+
+interface Props {
+  netflixOriginals: Movie[];
+}
+
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+
+  console.log(movie);
+
   return (
     <div>
-      <div>
-        <Image />
+      <div className="absolute top-0 left-0 h-[95vh] w-screen">
+        <Image
+          layout="fill"
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          objectFit="cover"
+        />
       </div>
     </div>
   );
