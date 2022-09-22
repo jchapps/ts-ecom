ea807bb1943183087958282a0a2924dbf5ac01aa
Josh Chappelow <jdchappelow@gmail.com>
2022-09-22 17:22:26 +0900

    heroicons

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { Movie } from "../typings";
 import { baseUrl } from "../constants/Movie";
 import Image from "next/image";
+import { FilmIcon } from "@heroicons/react/20/solid";
+import { InformationCircleIcon } from "@heroicons/react/20/solid";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -19,13 +21,31 @@ function Banner({ netflixOriginals }: Props) {
   console.log(movie);
 
   return (
-    <div>
+    <div className="flex flex-col space-y-10 py-16 md:space-y-10 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
           layout="fill"
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           objectFit="cover"
         />
+      </div>
+
+      <h1 className="text-2xl lg:text-7xl md:text-7xl font-bold">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+
+      <div>
+        <button className="bannerButton">
+          <FilmIcon />
+          Watch
+        </button>
+        <button className="bannerButton">
+          <InformationCircleIcon />
+          Info
+        </button>
       </div>
     </div>
   );
