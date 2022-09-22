6e84043d9aed4c38ca136b3d1026dc367d2d7ec5
Josh Chappelow <jdchappelow@gmail.com>
2022-09-22 18:02:50 +0900

    row component

---
+++ components/Row.tsx
@@ -0,0 +1,12 @@
+import { Movie } from "../typings";
+
+interface Props {
+  title: string;
+  movies: Movie[];
+}
+
+function Row({ title, movies }: Props) {
+  return <div>Row</div>;
+}
+
+export default Row;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import requests from "../utils/requests";
 import { Movie } from "../typings";
+import Row from "../components/Row";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -16,8 +17,16 @@ interface Props {
   documentaries: Movie[];
 }
 
-const Home = ({ netflixOriginals }: Props) => {
-  console.log(netflixOriginals);
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -27,6 +36,15 @@ const Home = ({ netflixOriginals }: Props) => {
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-24">
         <Banner netflixOriginals={netflixOriginals} />
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
       </main>
     </div>
   );
