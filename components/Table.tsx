f1571599202a09a5f0510868fdcde5b062ac6322
Josh Chappelow <jdchappelow@gmail.com>
2022-12-16 22:32:54 +0900

    feat: table product metadata

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,27 +1,49 @@
-import { Product } from '@stripe/firestore-stripe-payments'
-import React from 'react'
+import { Product } from "@stripe/firestore-stripe-payments";
+import React from "react";
 
 interface Props {
-  products: Product[]
+  products: Product[];
 }
 
-function Table({products}: Props) {
+function Table({ products }: Props) {
   return (
     <table>
-      <tbody>
-        <tr className='tableRow'>
-          <td className='tableDataTitle'>
-            Monthly Price
-          </td>
+      <tbody className="divide-y-3 divide-[gray]">
+        <tr className="tableRow">
+          <td className="tableDataTitle">Monthly Price</td>
           {products.map((product) => (
-            <td  className="tableData" key={product.id}>
+            <td className="tableData" key={product.id}>
               {product.prices[0].unit_amount}
+            </td>
+          ))}
+        </tr>{" "}
+        <tr className="tableRow">
+          <td className="tableDataTitle">Watch Limit</td>
+          {products.map((product) => (
+            <td className="tableData" key={product.id}>
+              {product.metadata.watchLimit} Trailers
+            </td>
+          ))}
+        </tr>
+        <tr className="tableRow">
+          <td className="tableDataTitle">Video Resolution</td>
+          {products.map((product) => (
+            <td className="tableData" key={product.id}>
+              {product.metadata.resolution}
+            </td>
+          ))}
+        </tr>
+        <tr className="tableRow">
+          <td className="tableDataTitle">Download</td>
+          {products.map((product) => (
+            <td className="tableData" key={product.id}>
+              {product.metadata.download}
             </td>
           ))}
         </tr>
       </tbody>
     </table>
-  )
+  );
 }
 
-export default Table
+export default Table;
