940529d2606eef01b64478242a2695e69a0a70ff
Josh Chappelow <jdchappelow@gmail.com>
2022-12-18 23:27:33 +0900

    refactor: new favicon

---
+++ pages/account.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+function account() {
+  return (
+    <div>
+      <Head>
+        <title>Your Icon</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
+
+export default account;
