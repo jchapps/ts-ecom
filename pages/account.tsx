38acb2eb2bc3a223e7e333de879ce4b513bbe94c
Josh Chappelow <jdchappelow@gmail.com>
2022-12-19 18:30:14 +0900

    fix: div order

--- pages/account.tsx
+++ pages/account.tsx
@@ -59,26 +59,30 @@ function account({ products }: Props) {
 
         <Membership/>
 
-        <div className="mt-6 grid grid-cols-1 gap-x4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0">
-          <h4>Plan Details:</h4>
-          <div>
+        <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0">
+          <h4 className="text-lg text-[gray]">Plan Details:</h4>
+          {/* Find the current plan */}
+          <div className="col-span-2 font-medium">
             {
               products.filter(
                 (product) => product.id === subscription?.product
               )[0]?.name
             }
           </div>
-          <p className="cursor-pointer text-blue-500 hover:underline md:text-right">
-            Change Plan
+          <p
+            className="cursor-pointer text-blue-500 hover:underline md:text-right"
+          >
+            Change plan
           </p>
         </div>
+
         <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
           <h4 className="text-lg text-[gray]">Settings</h4>
           <p
             className="col-span-3 cursor-pointer text-blue-500 hover:underline"
             onClick={logout}
           >
-            Sign out
+            Logout
           </p>
         </div>
       </main>
